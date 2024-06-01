cee5b3174691ef95587fe2d2b5f654f0f43808b7
angelajzhou <155860768+angelajmzhou@users.noreply.github.com>
2024-06-01 16:12:56 -0700

    Update hashtable_asm.s

--- hashtable_asm.s
+++ hashtable_asm.s
@@ -37,42 +37,39 @@ createHashTable:
 	sub rsp, 56				#maintain stack pointer alignment
 
 	#store saved registers rather than args on stack.
-	mov [rsp], rdi			#store arg1 (size)
-	mov [rsp+4], rsi		#store arg2 (hashFunction pointer)
-	mov [rsp+12], rdx		#arg3 (equalFunction pointer)
+	mov [rsp], r12d			
+	mov [rsp+4], r13		
+	mov [rsp+12], r14		
+	mov [rsp+20], r15		#saved reg. for newTable
 
-	mov [rsp+20], r12		#saved reg. for newTable
+	mov r12d, edi				#(int) size
+	mov r13, rsi				#(* hashFunction)
+	mov r14, rdx				#(* equalFunction)
 
-
-	mov edi, 32 			#size of hashtable
-	mov esi, 1	
+	mov rdi, 32 				#size of hashtable (arg1)
+	mov rsi, 1					#second arg (arg 2)
 	call calloc
-	mov r12, rax			#hashtable *newTable = malloc(sizeof(HashTable))
+	mov r15, rax				#hashtable *newTable = malloc(sizeof(HashTable))
 
-	mov r8, [rsp]
-	#offset = 8 + 8 + 8 = 24
-	mov [r12+24], r8 		#newTable->size = size
+	mov [r15+24], r12d 			#newTable->size = size
+	mov [r15+0], r13			#newTable->hashFunction = hashFunction;
+	mov [r15+8], r14			#newTable->equalFunction = equalFunction;
+	mov dword ptr [r15+28], 0 	#newTable->used = 0 | store as 32b register
 
-	mov eax, 8				#size of hashbucket pointer
-	mul r8					#hashbucket * size
+	mov eax, 8					#size of hashbucket pointer
+	mul r8						#hashbucket * size
 	mov esi, 1
 
 	call calloc
-	mov [r12+16], rax		#newTable->data = malloc (size of...)
+	
+	mov [r15+16], rax		#newTable->data = malloc (size of...)
 
-	mov r10, 0				#initialize the counter variable to scratch reg
+	mov rax, r15				#move newTable to return
 
-	mov r12, [rsp+4]		#newTable->hashFunction = hashFunction;
-
-	mov r8, [r12+8]
-	mov r8, [rsp+12]		#newTable->equalFunction = equalFunction;
-
-	mov rax, r12				#move newTable to return
-
-	mov rdi, [rsp]			#store arg1 (size)
-	mov rsi, [rsp+4]		#store arg2 (hashFunction pointer)
-	mov rdx, [rsp+12]		#arg3 (equalFunction pointer)
-	mov r12, [rsp+20]		#saved reg. for newTable
+	mov r12d, [rsp]
+	mov r13, [rsp+4]
+	mov r14, [rsp+12]	
+	mov r15, [rsp+20]	#saved reg. for newTable
 
 	add rsp, 56
 	ret
@@ -81,60 +78,59 @@ createHashTable:
 insertData:
 	sub rsp, 56
 
-	#store the arguments on stack
-	mov [rsp], rdi
-	mov [rsp+8], rsi
-	mov [rsp+16], rdx
+	#store the saved registers
+	mov [rsp], r12
+	mov [rsp+8], r13
+	mov [rsp+16], r14
 
 	#saved register for newBucket
-	mov [rsp+24], r12	
+	mov [rsp+24], r15	
+
+	#move args into saved registers
+
+	mov r12, rdi				#(* table)
+	mov r13, rsi				#(* key)
+	mov r14, rdx				#(* data)
 
 	#malloc space for newBucket
-	mov edi, 24
+	mov rdi, 24
 	call malloc
-
-	mov r12, rax					#r12 = newBucket
+	mov r15, rax					#r15 = newBucket
 
 	#get hashFunction
-	mov r10, [rsp]					#deref to get hashtable
-	mov r10, [r9]					#deref to get hashFunction
+	mov r10, [r12]				#deref to get hashFunction
 	
 	#get the key
-	mov r11, [rsp+8]				#get key off stack
-	mov rdi, [r11]					#double deref for key
+	mov rdi, [r13]					#get key pointer
 	
 	#call hashFunction
-	call r10						#rax = hashFunctin(key)
+	call r10						#rax = hashFunction(key)
 
-	mov r11, [rsp]					#get table
-	mov	r11, [r11+24]				#get table->size
-
-	div r11							#((table->hashFunction)(key)) % table->size
+	mov	r11d, [r12+24]				#r11 =  table->size
+	mov rdx, 0						#zero out upper bits
+	div r11d							#((table->hashFunction)(key)) % table->size
 	#remainder is in rdx || location = rdx
 	#both sides can't have brackets
-	mov r10, [rsp]
-	mov r10, [r10+16]				#deref twice to get table->data
+	mov r10, [r12+16]				#r10 = table->data
 
-	mov r8, [r12+16]
-	mov r8, [r10 + 8*rdx]		#newBucket->next = table->data[location]
+	mov r11, [r10+8*rdx]			#r11 = table->data[location]
+	mov [r15+16], r11			#newBucket -> next =table->data[location] 
 
-	mov r8, [r12+8]
-	mov r8, [rsp+16]			#newBucket -> data = data	
+	mov [r15+8], r14			#newBucket -> data = data	
 
-	mov r8, [r12]
-	mov r8, [rsp+8]				#newBucket -> key = key
+	mov [r15], r13				#newBucket -> key = key
 
-	mov [r10+8*rdx], r12		#table->data[location] = newBucket
+	mov [r10+8*rdx], r15		#table->data[location] = newBucket
 
-	mov r10, [rsp]					#deref hashTable
-	mov r9d, [r10+28]			#get hashTable->used
+	mov r9d, [r12+28]			#get hashTable->used
 	add r9d, 1					#increment by 1
-	mov [r10+28], r9			#update hashTable->used
+	mov [r10+28], r9d			#update hashTable->used
 
-	mov rdi, [rsp]			#store arg1 (size)
-	mov rsi, [rsp+4]		#store arg2 (hashFunction pointer)
-	mov rdx, [rsp+12]		#arg3 (equalFunction pointer)
-	mov r12, [rsp+20]		#saved reg. for newTable
+	#restore registers
+	mov r12, [rsp]
+	mov r13, [rsp+8]
+	mov r14, [rsp+16]	
+	mov r15, [rsp+24]
 	
 	add rsp, 56
 	ret
@@ -143,58 +139,59 @@ insertData:
 findData:
 	sub rsp, 24				#maintain stack pointer alignment
 
-	mov [rsp], rdi			#store arg1 (hashTable pointer)
-	mov [rsp+8], rsi		#store arg2 (key pointer)
-	mov [rsp+16], r12			#saved register (for lookAt)
+	mov [rsp], r12
+	mov [rsp+8], r13
+	mov [rsp+16], r14
+
+	mov r12, rdi				#(* table)
+	mov r13, rsi				#(* key)
 
 	#get hashFunction
-	mov r10, [rsp]					#deref to get hashtable
+	mov r10, [r12]					#deref to get hashFunction
 	
 	#get the key
-	mov r11, [rsp+8]				#get key off stack
-	mov rdi, [r11]					#rdi = double deref for key
+	mov rdi, r13					#move key to arg1
 	
 	#call hashFunction
-	call [r10]						#rax = hashFunction(key)
+	call r10						#rax = hashFunction(key)
 
-	mov r11, [rsp]					#get table
-	mov	r11, [r11+24]				#get table->size
-
-	div r11							#((table->hashFunction)(key)) % table->size
+	mov r11d, [r12+24]				#get table->size
+									#takes rax as parameter
+	mov rdx, 0
+	div r11d						#((table->hashFunction)(key)) % table->size
 									#remainder is in rdx || location = rdx
 
 #  struct HashBucket *lookAt = table->data[location];
-	mov r10, [rsp]					#deref for hashTable
-	mov r10, [r10+16]				#deref to get table->data
-
-
-	mov r12, [r10+8*rdx]			#lookAt = table->data[location]
+	mov r10, [r12+16]				#r11 = table->data
+	shl rdx, 3
+	add r10, rdx
+	mov r14, r10
+	# mov r14, [r10+8*rdx]			#lookAt = table->data[location]
 
 while_start:
 	cmp r12, 0
 	je while_end
-	mov rdi, [rsp+8] 				#prep 1st argument (key)
-	mov r12, [r12]					#1st deref: get hashBucket lookAt
-	mov rsi, [r12]					#2nd deref: prep 2nd argument	(lookAt->key)
+	mov rdi, r13					#arg1: key
+	mov rsi, [r14]				#arg2: lookAt->key : invalid read
 
 	#get equalFunction
-	mov r10, [rsp]					#deref to get hashTable
-	mov r10, [r10+8]				#deref to get equalFunction
+	#mov r10, [r12+8]				#r10 = equalFunction
 
 	#call equalFunction
-	call [r10]						#rax = equalFunction(key, lookAt->key)
+	call [r12+8]						#rax = equalFunction(key, lookAt->key)
+	#invalid read of size 1....
 
 	cmp rax, 0
-	cmovnz rax, [r12+8]				#lookAt -> data
-	jnz epilogue
+	cmovne rax, [r14+8]				#lookAt -> data
+	jne epilogue
 
-	mov r12, [r12+16]				#lookAt = lookAt -> next
+	mov r14, [r14+16]				#lookAt = lookAt -> next
 while_end:
 	mov rax, 0
 epilogue:
-	mov rdi, [rsp]			
-	mov rsi, [rsp+8]		
-	mov r12, [rsp+16]		
+	mov r12, [rsp]
+	mov r13, [rsp+8]
+	mov r14, [rsp+16]	
 	
 	add rsp, 24
 	ret
